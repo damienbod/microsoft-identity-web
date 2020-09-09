c6abdfa0e2a8faa9aab80d447d6c745bfac8f2b7
pmaytak <34331512+pmaytak@users.noreply.github.com>
2020-09-08 22:01:11 -0700

    Perf project.

---
+++ tests/Perf/Microsoft.Identity.Web.Perf.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Identity.Web.Perf.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
